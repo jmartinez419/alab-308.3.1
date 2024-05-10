c5af9111a6cbac37127ba583c2b95b952ffa6316
jmartinez419 <justynmartinez419@gmail.com>
2024-05-10 16:20:56 -0400

    made a function to find prime numbers and then made a loop that uses said function until it finds the next prime number

--- app.js
+++ app.js
@@ -13,4 +13,28 @@ function log(num) {
       
   }
 }
-log (100)
+
+function checkPrime(num) {
+  let i, flag = true;
+  for (i = 2; i <= num - 1; i++) {
+      if (num % i == 0) {
+          flag = false;
+          break;
+      }
+  }
+  if (flag == true)
+      console.log(num + " is prime");
+  else
+      console.log(num + " is not prime");
+}
+
+let n = 10;
+
+while (true) {
+  let nextNum = n + 1;
+  if (checkPrime(nextNum)) {
+    console.log(nextNum);
+    break;
+  }
+  n++;
+}
